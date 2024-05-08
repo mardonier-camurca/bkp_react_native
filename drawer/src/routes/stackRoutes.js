8acf70b29ff7a6d413a63919dc38850c9a588ca0
mardonier-camurca <159277473+mardonier-camurca@users.noreply.github.com>
2024-05-08 13:36:52 -0300

    updated backup folder

---
+++ drawer/src/routes/stackRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+import Home from '../pages/Home';
+import Detalhes from '../pages/Detalhes';
+import { View } from 'react-native';
+
+
+
+export default function StackRoutes(){
+    return(
+        <View>
+            
+        </View>
+    )
+}
+
